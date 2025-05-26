09a2eee67b0eb998ab5a8a3d3024077479f773b0
ginger <jum80frol@gmail.com>
2025-05-26 11:52:01 +0600

    dep 5

--- project/settings.py
+++ project/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.parse(os.getenv('DATABASE_URL')),
-    # 'default': {
+    # 'default': {s
     #     'ENGINE': 'django.db.backends.postgresql',
     #     'NAME': os.getenv("DB_NAME"),
     #     'USER': os.getenv("DB_USER"),
